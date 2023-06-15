be1c3edaed815311dcde483877273a0d889aa8db
Thomas MG Legalche <thomas.legalche@gmail.com>
2023-06-15 05:15:27 +0200

    enum typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
-  return <div className="app">hello</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
